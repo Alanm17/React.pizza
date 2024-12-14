5480049a50732540d4dc2c4c80752b2f932a0067
Alan <Alan2003@sju.ac.kr>
2024-12-14 23:44:09 +0900

    destructing objects try to follow DRY with typescript & JSX

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,20 @@
 import React from "react";
-import { PizzaType } from "./types.ts"; // Import TypeScript types
-import { pizzaData } from "./data.ts"; // Import Pizza data
+import ReactDOM from "react-dom/client";
+import { PizzaType } from "./types"; // Import TypeScript types
+import { pizzaData } from "./data"; // Import Pizza data
 import "./index.css"; // Importing styles
 
 // Header Component
+export function App() {
+  return (
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
 export function Header() {
   return (
     <header className="header">
@@ -15,38 +26,48 @@ export function Header() {
 // Footer Component
 export function Footer() {
   const hour = new Date().getHours();
-  const openHours = 12;
+  const openHours = 11;
   const closedHours = 24;
   const isOpen = hour >= openHours && hour <= closedHours;
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>
             We're open until {closedHours}:00. Come visit us or order online!
           </p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          {" "}
+          We are happy to welcome you between{openHours}:00{closedHours}:00
+        </p>
       )}
     </footer>
   );
 }
 
 // Pizza Component
-type PizzaProps = {
+type pizzaProps = {
   pizzaObj: PizzaType;
 };
-
-export function Pizza({ pizzaObj }: PizzaProps) {
+//we can use if condtions for early returns if the condition does match ;
+//ex:
+export function Pizza({ pizzaObj }: pizzaProps) {
+  if (pizzaObj.soldOut) return null; // null yozishni esdan chqarma mazgi bolmasa undefined qaytaradi return o'zi
   return (
     <div className="pizza">
       <img className="pizza-img" src={pizzaObj.photoName} alt={pizzaObj.name} />
       <div>
         <h3>{pizzaObj.name}</h3>
         <p>{pizzaObj.ingredients}</p>
-        <span className="price">${pizzaObj.price}</span>
-        {pizzaObj.soldOut && <span className="sold-out">Sold Out</span>}
+        {pizzaObj.soldOut ? (
+          <span className="sold-out">Sold Out</span>
+        ) : (
+          <span className="price">${pizzaObj.price}</span>
+        )}
       </div>
     </div>
   );
@@ -57,11 +78,25 @@ export function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <h1 className="text-4xl text-blue-500">
+        Tailwind CSS is working in React!
+      </h1>
+
       <ul className="pizzas">
         {pizzaData.map((pizza) => (
-          <Pizza pizzaObj={pizza} key={pizza.name} />
+          <Pizza pizzaObj={pizza} />
         ))}
       </ul>
     </main>
   );
 }
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
